4350740f63fbd94b0397e1e7ba9a50e22c11658c
wkd-woo <wkdwoos@gmail.com>
2022-06-10 17:01:18 +0900

    node.js get post + middleware(express.json())

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodejs-mongo
----
+
+Node.js와 MongoDB을 학습해보자
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', function(req, res){
-    return res.send('hello world!');
+const users = [];
+
+
+app.use(express.json());
+
+app.get('/user', function(req, res){
+    return res.send({users:users});
+})
+
+app.post('/user', function(req, res){
+    console.log(req.body)
+
+    users.push({name:req.body.name, age:req.body.age})
+    return res.send({success:true})
 })
 
 app.listen(3000, function(){
