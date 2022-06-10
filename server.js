575efe3ef9b649f8d8186fce614a220f142dc17b
wkd-woo <wkdwoos@gmail.com>
2022-06-10 21:31:17 +0900

    mongodb-nodejs got connection. and writing .env

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const users = [];
 
-app.use(express.json());
+const server = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-app.get("/user", function (req, res) {
-  // 기본 폼
-  return res.send({ users: users });
-});
+    app.use(express.json());
 
-app.post("/user", (req, res) => {
-  // arrow function
-  console.log(req.body);
+    app.get("/user", function (req, res) {
+      // 기본 폼
+      return res.send({ users: users });
+    });
 
-  users.push({ name: req.body.name, age: req.body.age });
-  return res.send({ success: true });
-});
+    app.post("/user", (req, res) => {
+      // arrow function
+      console.log(req.body);
 
-app.listen(3000, () => {
-  console.log("server listening on port 3000");
-});
+      users.push({ name: req.body.name, age: req.body.age });
+      return res.send({ success: true });
+    });
+
+    app.listen(3000, () => {
+      console.log("server listening on port 3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+server();
