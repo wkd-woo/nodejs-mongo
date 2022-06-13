1897164fbb18187ab37e97a0b4eb99508ec98f33
wkd-woo <wkdwoos@gmail.com>
2022-06-14 00:35:20 +0900

    router 리팩토링 + save로 수정-> 변경감지

---
+++ src/routes/userRoute.js
@@ -0,0 +1,105 @@
+const { Router } = require('express')
+const userRouter = Router()
+const mongoose = require('mongoose')
+const { User } = require('../models/User')
+
+userRouter.get('/', async (req, res) => {
+  try {
+    const users = await User.find({})
+    return res.send({ users })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+userRouter.get('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params
+    if (!mongoose.isValidObjectId(userId))
+      return res.status(400).send({ err: 'invalid userId' })
+
+    const user = await User.findOne({ _id: userId })
+    return res.send({ user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+userRouter.post('/', async (req, res) => {
+  try {
+    let { username, name } = req.body
+    if (!username) return res.status(400).send({ err: 'username is required' })
+    if (!name || !name.first || !name.last)
+      return res
+        .status(400)
+        .send({ err: 'Both first and last names are required' })
+
+    const user = new User(req.body)
+    await user.save()
+    return res.send({ user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+userRouter.delete('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params
+    if (!mongoose.isValidObjectId(userId))
+      return res.status(400).send({ err: 'invalid userId' })
+    const user = await User.findOneAndDelete({ _id: userId })
+    return res.send({ user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+userRouter.put('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params
+    if (!mongoose.isValidObjectId(userId))
+      return res.status(400).send({ err: 'invalid userId' })
+
+    const { age, name } = req.body
+    if (!age && !name)
+      return res.status(400).send({ err: 'age or name is required' })
+    if (age && typeof age !== 'number')
+      return res.status(400).send({ err: 'age must be a number' })
+    if (
+      name &&
+      typeof name.first !== 'string' &&
+      name &&
+      typeof name.first !== 'string'
+    )
+      return res
+        .status(400)
+        .send({ err: 'first and last name are must be a number' })
+
+    // let updateBody = {}
+    // if (age) updateBody.age = age
+    // if (name) updateBody.name = name
+
+    // const user = await User.findByIdAndUpdate(
+    //   userId,
+    //   { $set: { age, name } },
+    //   { new: true }
+    // )
+
+    let user = await User.findById(userId)
+    if (age) user.age = age
+    if (name) user.name = name
+    await user.save()
+    return res.send({ user })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+module.exports = {
+  userRouter,
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { userRouter } = require('./routes/userRoute')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const { User } = require('./models/User')
@@ -8,86 +9,9 @@ const server = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Mongodb connected')
-
     app.use(express.json())
 
-    app.get('/user', async (req, res) => {
-      try {
-        const users = await User.find({})
-        return res.send({ users })
-      } catch (err) {
-        console.log(err)
-        return res.status(500).send({ err: err.message })
-      }
-    })
-
-    app.get('/user/:userId', async (req, res) => {
-      try {
-        const { userId } = req.params
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: 'invalid userId' })
-
-        const user = await User.findOne({ _id: userId })
-        return res.send({ user })
-      } catch (err) {
-        console.log(err)
-        return res.status(500).send({ err: err.message })
-      }
-    })
-
-    app.post('/user', async (req, res) => {
-      try {
-        let { username, name } = req.body
-        if (!username)
-          return res.status(400).send({ err: 'username is required' })
-        if (!name || !name.first || !name.last)
-          return res
-            .status(400)
-            .send({ err: 'Both first and last names are required' })
-        const user = new User(req.body)
-        await user.save()
-        return res.send({ user })
-      } catch (err) {
-        console.log(err)
-        return res.status(500).send({ err: err.message })
-      }
-    })
-
-    app.delete('/user/:userId', async (req, res) => {
-      try {
-        const { userId } = req.params
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: 'invalid userId' })
-        const user = await User.findOneAndDelete({ _id: userId })
-        return res.send({ user })
-      } catch (err) {
-        console.log(err)
-        return res.status(500).send({ err: err.message })
-      }
-    })
-
-    app.put('/user/:userId', async (req, res) => {
-      try {
-        const { userId } = req.params
-        if (!mongoose.isValidObjectId(userId))
-          return res.status(400).send({ err: 'invalid userId' })
-
-        const { age } = req.body
-        if (!age) return res.status(400).send({ err: 'age is required' })
-        if (typeof age !== 'number')
-          return res.status(400).send({ err: 'age must be a number' })
-
-        const user = await User.findByIdAndUpdate(
-          userId,
-          { $set: { age } },
-          { new: true }
-        )
-        return res.send({ user })
-      } catch (err) {
-        console.log(err)
-        return res.status(500).send({ err: err.message })
-      }
-    })
+    app.use('/user', userRouter)
 
     app.listen(3000, () => console.log('server listening on port 3000'))
   } catch (err) {
