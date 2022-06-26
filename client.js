bb46e88fb68aea23cb2409b6210aeb16279b1610
wkd-woo <wkdwoos@gmail.com>
2022-06-26 23:49:55 +0900

    nesting 성능 테스트

--- client.js
+++ client.js
@@ -5,13 +5,18 @@ console.log('client code running.')
 
 const test = async () => {
   console.time('loading time: ')
-  let data = await axios.get(`${URI}/user`)
-  console.log(data.users)
+  let data = await axios.get(`${URI}/blog`)
+  // console.log(data.users)
   console.timeEnd('loading time: ')
 }
 
 const testGroup = async () => {
   await test()
+  await test()
+  await test()
+  await test()
+  await test()
+  await test()
 }
 
-test()
+testGroup()
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const { userRouter, blogRouter } = require('./routes')
 const mongoose = require('mongoose')
-const { generateFakeData } = require('../faker')
+const { generateFakeData } = require('../faker2')
 
 require('dotenv').config()
 
@@ -10,13 +10,17 @@ const server = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
 
-    //generateFakeData(100, 10, 300)
     console.log('Mongodb connected')
     app.use(express.json())
 
     app.use('/user', userRouter)
     app.use('/blog', blogRouter)
-    app.listen(3000, () => console.log('server listening on port 3000'))
+    app.listen(3000, async () => {
+      console.log('server listening on port 3000')
+      // for (i = 0; i < 20; i++) {
+      //   await generateFakeData(10, 1, 10)
+      // }
+    })
   } catch (err) {
     console.log(err)
   }
